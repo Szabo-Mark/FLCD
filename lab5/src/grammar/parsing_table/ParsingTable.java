27037ec417c2fdd0e442b336d9473a5dcf5fa809
Mark <mark.szabo@nagarro.com>
2022-12-14 14:47:52 +0200

    isa

--- lab5/src/grammar/parsing_table/ParsingTable.java
+++ lab5/src/grammar/parsing_table/ParsingTable.java
@@ -1,11 +1,35 @@
 package grammar.parsing_table;
 
-import java.util.Map;
+import grammar.Production;
+
+import java.util.*;
 
 public class ParsingTable {
-    Map<String, Map<String, ParsingTableCell>> parsingTable;
+    private final Set<String> nonTerminals;
+    private final Set<String> terminals;
+    private final Set<Production> productions;
+    private Map<String, Map<String, ParsingTableCell>> parsingTable;
 
-    public ParsingTableCell get(String firstSymbol, String secondSymbol){
+    public ParsingTable(Set<String> nonTerminals, Set<String> terminals, Set<Production> productions) {
+        this.nonTerminals = nonTerminals;
+        this.terminals = terminals;
+        this.productions = productions;
+        parsingTable = new HashMap<>();
+        Set<String> symbols = new HashSet<>(nonTerminals);
+        symbols.addAll(terminals);
+        for(String symbol : symbols){
+            for(String terminal : terminals){
+            }
+        }
+        put("$", "$", new ParsingTableCell(List.of("ACC"), 0));
+    }
+
+
+    public ParsingTableCell get(String firstSymbol, String secondSymbol) {
         return parsingTable.get(firstSymbol).get(secondSymbol);
+    }
+
+    public void put(String firstSymbol, String secondSymbol, ParsingTableCell parsingTableCell) {
+        parsingTable.get(firstSymbol).put(secondSymbol, parsingTableCell);
     }
 }
